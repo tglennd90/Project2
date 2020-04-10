3be1ef5b8a3b3d04e853a0a913c5668ca4642b68
Jacob Tate <jacob87608@gmail.com>
2020-04-09 21:52:45 -0500

    the sell function is now working

--- public/js/index.js
+++ public/js/index.js
@@ -7,23 +7,19 @@ $.ajax({
         if (makeArr.indexOf(res.cars[i].make) === -1) {
             makeArr.push(res.cars[i].make);
         };
+        
     };
     for (let j = 0; j < makeArr.length; j++) {
-        //++++++++++++++++++++++++++++++++++++++++
-        //populates the makes for the buy page
         $("#cars").append($("<option>", {
             value: makeArr[j],
             text: makeArr[j]
         }));
-        //+++++++++++++++++++++++++++++++++++++++++
-        //populates the makes for the sell page
         $("#carsSell").append($("<option>", {
             value: makeArr[j],
             text: makeArr[j]
         }));
+
     };
-    //===========================================
-    //the buy page logic
     $("#cars").on("change", function () {
         $("#models").empty();
         $("#models").append($("<option>", {
@@ -40,18 +36,28 @@ $.ajax({
             url: "/api/cars",
             data: selectedObj,
         }).then(function (res) {
+            var carResults = [];
+
             for (let j = 0; j < res.cars.length; j++) {
                 $("#models").append($("<option>", {
                     value: res.cars[j].model,
                     text: res.cars[j].model
                 }));
+
+                carResults.push(res.cars[j].make, res.cars[j].model, res.cars[j].color, res.cars[j].carYear, res.cars[j].miles, res.cars[j].price, res.cars[j].imageLink);
             }
             $("#buySubmit").on("click", function (e) {
                 e.preventDefault();
-
+                console.log(carResults[6]);
+                var carItemDisplay = $(".carItemDisplay");
+                var image = $("<img>");
+                image.attr("class", "carPhoto");
+                $(image).attr("src", carResults[6]);
+                carItemDisplay.append(image);
             });
         });
     });
+});
     //================================================
     //the sell page logic
     function sell() {
@@ -124,4 +130,3 @@ $.ajax({
         e.preventDefault();
         sell();
     });
-});
